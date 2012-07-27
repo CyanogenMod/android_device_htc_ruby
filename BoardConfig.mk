f903a0c07f3627b357d662a5211c77aaab4cc75c
faux123 <reioux@gmail.com>
2012-07-26 20:42:45 -0500

    BoardConfig.mk: update to use real ruby setting

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,7 @@ TARGET_BOOTLOADER_BOARD_NAME := ruby
 
 # Kernel
 BOARD_KERNEL_BASE := 0x48000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0 androidboot.hardware=ruby no_console_suspend=1
+BOARD_KERNEL_CMDLINE := console=ttyHSL3 androidboot.hardware=ruby no_console_suspend=1
 BOARD_KERNEL_PAGE_SIZE := 2048
 TARGET_KERNEL_CONFIG := ruby_defconfig
 TARGET_PREBUILT_KERNEL := device/htc/ruby/prebuilt/kernel
