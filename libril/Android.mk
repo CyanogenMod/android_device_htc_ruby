3fe989a711e5875e33fc69fc6db81dd650a27306
Ethan Chen <intervigil@gmail.com>
2012-11-26 23:17:11 -0800

    ruby: only build libril if BOARD_PROVIDES_LIBRIL is true

--- libril/Android.mk
+++ libril/Android.mk
@@ -1,5 +1,6 @@
 # Copyright 2006 The Android Open Source Project
 
+ifeq ($(BOARD_PROVIDES_LIBRIL),true)
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),ruby)
 
 LOCAL_PATH:= $(call my-dir)
@@ -24,3 +25,4 @@ LOCAL_LDLIBS += -lpthread
 include $(BUILD_SHARED_LIBRARY)
 
 endif # BOARD_BOOTLOADER_BOARD_NAME
+endif # BOARD_PROVIDES_LIBRIL
