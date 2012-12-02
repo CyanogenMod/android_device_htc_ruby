46deca2327f05dda7c80b53a3bdb5554ba7fef08
Ethan Chen <intervigil@gmail.com>
2012-12-01 23:22:56 -0800

    ruby: use common GPS config from msm8660-common

--- device_ruby.mk
+++ device_ruby.mk
@@ -26,9 +26,6 @@ DEVICE_PACKAGE_OVERLAYS += device/htc/ruby/overlay
 PRODUCT_PACKAGES += \
     gps.ruby
 
-## The gps config appropriate for this device
-PRODUCT_COPY_FILES += device/common/gps/gps.conf_EU:system/etc/gps.conf
-
 # NFC
 PRODUCT_PACKAGES += \
     libnfc \
